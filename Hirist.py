365fbf316f36635e1a7b50d13843a4f4f9afc6eb
nikhilramgiri <63799117+nikhilramgiri@users.noreply.github.com>
2021-04-06 16:02:54 +0530

    options update in hirist

--- Hirist.py
+++ Hirist.py
@@ -52,11 +52,22 @@ for i in range(len(copy_data)):
 d = {'role':'Developer','Location':['Mumbai','Banglore']}
 
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument(f'user-agent={user_agent}')
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument("--disable-extensions")
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+options.add_argument("--start-maximized")
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
 browser.maximize_window()
 browser.get('https://recruit.hirist.com/login')
 
